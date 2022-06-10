ccbc39097162f78f4fdcf5b448b6dd0002b691c2
jrmm93 <jeremyatn@gmail.com>
2022-06-10 17:23:11 +0200

    feat: added breakpoint mediaquery for mobile version on homepage's nav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />  
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
