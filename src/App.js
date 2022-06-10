4929f397b8214418f1e7dee011d8613f8e2332b3
Adlen <adlen.bouhassane@laposte.net>
2022-06-10 10:44:52 +0200

    first commit homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Logo from './Logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Logo} className="App-logo" alt="logo" />
+        <h1>Microscope-Web</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
