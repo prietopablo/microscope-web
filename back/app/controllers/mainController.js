12bc697a4ab45211003c8e68405c6166b6c935d5
PrietoPablo <prietopablo88@gmail.com>
2022-06-11 16:07:41 +0200

    fixed: user insert method feat: http server implementation

--- back/app/controllers/mainController.js
+++
@@ -1,9 +0,0 @@
-const mainController = { 
-    homePage : (request, response) => {
-        console.log('Ceci est un test');
-
-        response.send('Ceci est un test pour le Front');
-    },
-};
-
-module.exports = mainController;
--- back/app/models/client.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require('pg');
-require('dotenv').config();
-
-const client = new Client(process.env.PG_URL);
-
-client.connect();
-
-module.exports = client;
