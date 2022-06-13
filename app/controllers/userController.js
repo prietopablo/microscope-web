1a3563d15f746cbd1d60504cc7cfdedb1628c22a
PrietoPablo <prietopablo88@gmail.com>
2022-06-13 10:29:37 +0200

    feat: getOne method for user table

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -8,6 +8,15 @@ const userController = {
         response.json({ userList });
     },
 
+    async getOne(request, response) {
+        console.log(request.params.id);
+        const user = await userDatamapper.findByPk(request.params.id);
+        
+        // WE need res a 404 if there is no user at this primary key
+
+        return response.json({ user });
+    },
+
     async create(request, response) {
         // We call for the body
         const newUser = request.body;
@@ -16,6 +25,15 @@ const userController = {
         const userList = await userDatamapper.findAll();
         response.json({ userList });
     },
+
+    async delete(request, response) {
+
+    },
+
+    async update(request, response) {
+        
+    },
+
 };
 
 module.exports = userController;
