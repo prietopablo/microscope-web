a0fe12ea024b0cc8329f3783201169a6f0ac9418
PrietoPablo <prietopablo88@gmail.com>
2022-06-13 14:27:16 +0200

    feat: CRUD for the user relation complete & isUnique method done(we have used the 'Object.entries()' method

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -12,26 +12,86 @@ const userController = {
         console.log(request.params.id);
         const user = await userDatamapper.findByPk(request.params.id);
         
-        // WE need res a 404 if there is no user at this primary key
+        if (!user) {
+            return response.status(404).json();
+        }
 
         return response.json({ user });
     },
 
     async create(request, response) {
         // We call for the body
-        const newUser = request.body;
-        await userDatamapper.insert(newUser);
-        // We display the user list with the new data
-        const userList = await userDatamapper.findAll();
-        response.json({ userList });
+        const user = await userDatamapper.isUnique(request.body);
+        if (user) {
+            let field;
+            if (user.username === request.body.username) {
+                field = 'username';
+            } else {
+                field = 'email';
+            }
+            return response.status(400).json({ errorMessage: `Other user already exists with this ${field}` });
+            
+        }
+
+        const savedUser = await userDatamapper.insert(request.body);
+        return response.json(savedUser);
+    
     },
 
     async delete(request, response) {
+        const user = await userDatamapper.findByPk(request.params.id);
 
+        if (!user) {
+            return response.status(404).json();
+        }
+
+        await userDatamapper.delete(request.params.id);
+        return response.status(204).json();
     },
 
     async update(request, response) {
-        
+        const user = await userDatamapper.findByPk(request.params.id);
+
+        if (!user) {
+            return response.status(404).json();
+        }
+
+        /*if (request.body.username || request.body.email) {
+            const existingUser = await userDatamapper.isUnique(request.body, request.params.id);
+            
+            const existingStatus = {
+                usernameStatus: "no update",
+                emailStatus: "no update"
+            };
+
+            if (!existingUser.isUniqueUsername) {
+                existingStatus.usernameStatus = `Other user already exists with this username`;
+                
+            } if (!existingUser.isUniqueEmail) {
+                existingStatus.emailStatus = `Other user already exists with this email`;
+                
+            } else {
+                const savedUser = await userDatamapper.update(request.body, request.params.id);
+                return response.json(savedUser);
+            }
+            return response.json(existingStatus);            
+        }*/
+
+        if (request.body.username || request.body.email) {
+            const existingUser = await userDatamapper.isUnique(request.body, request.params.id);
+            if (existingUser) {
+                let field;
+                if (existingUser.username === request.body.username) {
+                    field = 'username';
+                } else {
+                    field = 'email';
+                }
+                return response.status(400).json({ errorMessage: `Other user already exists with this ${field}` });
+            }
+        }
+
+        const savedUser = await userDatamapper.update(request.params.id, request.body);
+        return response.json(savedUser);
     },
 
 };
--- app/models/userDatamapper.js
+++ app/models/userDatamapper.js
@@ -33,6 +33,80 @@ const userDatamapper = {
         return newUser.rows[0];
 
     },
+
+    async delete(userId) {
+        const result = await client.query('DELETE FROM "user" WHERE "id" = $1', [userId]);
+
+        return !!result.rowCount;
+    },
+
+    async update(inputData, userId) {
+        const fields = Object.keys(inputData).map((prop, index) => `"${prop}" = $${index + 1}`);
+        const values = Object.values(inputData);
+
+        const savedUser = await client.query(
+            `
+                UPDATE "user" SET
+                    ${fields}
+                WHERE id = $${fields.length + 1}
+                RETURNING *
+            `,
+            [...values, userId],
+        );
+
+        return savedUser.rows[0];
+    },
+
+    async isUnique (inputData, userId) {
+        /*
+        const results = {
+            isUniqueUsername: true,
+            isUniqueEmail: true
+        };
+        if (inputData.username) {
+            const result = await client.query('SELECT * FROM "user" WHERE "username"= $1', [inputData.username]);
+            if (result.rows !== 0) {
+                result.isUniqueUsername = false;
+            }
+        }
+        if (inputData.email) {
+            const result = await client.query('SELECT * FROM "user" WHERE "email"= $1', [inputData.email]);
+            if (result.rows !== 0) {
+                result.isUniqueEmail = false;
+            }
+        }
+        return results;
+        */
+
+        const fields = [];
+        const values = [];
+        // We gather our inputData
+        Object.entries(inputData).forEach(([key, value], index) => {
+            // We keep the data that are supposed to be unique
+            if (['username', 'email'].includes(key)) {
+                // We generate filters
+                fields.push(`"${key}" = $${index + 1}`);
+                values.push(value);
+            }
+        });
+
+        const preparedQuery = {
+            text: `SELECT * FROM "user" WHERE (${fields.join(' OR ')})`,
+            values,
+        };
+        // If an id is specified, we exclude its registered row
+        if (userId) {
+            preparedQuery.text += ` AND id <> $${values.length + 1}`;
+            preparedQuery.values.push(userId);
+        }
+        const result = await client.query(preparedQuery);
+
+        if (result.rowCount === 0) {
+            return null;
+        }
+
+        return result.rows[0];
+    }
 };
 
 module.exports = userDatamapper;
