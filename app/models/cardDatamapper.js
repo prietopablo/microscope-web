e64f58ce3f56545980a887fa36a2445b86aea951
Victor <vict74.ferreira@gmail.com>
2022-06-15 08:56:14 +0200

    feat: create new branch

--- app/models/cardDatamapper.js
+++ app/models/cardDatamapper.js
@@ -3,7 +3,7 @@ const client = require('./client');
 const cardDatamapper = {
    async insertPeriod(content) {
       const preparedQuery = {
-         text : `INSERT INTO "period"`,
+         text : `INSERT INTO "period" ("text", "period_tone", "position")`,
          values:
       }
    }
