c31d1710747151c1d01624fe41e938c5a4f1d806
jrmm93 <jeremyatn@gmail.com>
2022-06-16 15:54:32 +0200

    feat: nav changes when user is connected and logout button should be working

--- src/components/HomePage/HomePageLinks/HomePageLinks.js
+++ src/components/HomePage/HomePageLinks/HomePageLinks.js
@@ -1,18 +1,32 @@
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { useNavigate } from 'react-router-dom';
 import './HomePageLinks.css';
 
-function HomePageLinks() {
+function HomePageLinks({
+    handleLogout,
+}) {
     
     const navigate = useNavigate();
+    const isConnected = useSelector((state) => state.isConnected);
     
     return(
-        <div className="nav">
-            <Button inverted className="nav--link" onClick={() => navigate('/login')}>Se connecter</Button>
-            <Button inverted className="nav--link" onClick={() => navigate('/signup')}>Inscription</Button>
-        </div>
-
+        !isConnected ?
+            <div className="nav">
+                <Button inverted className="nav--link" onClick={() => navigate('/login')}>Se connecter</Button>
+                <Button inverted className="nav--link" onClick={() => navigate('/signup')}>Inscription</Button>
+            </div>
+        :
+            <div className="nav">
+                <Button inverted className="nav--link" onClick={() => navigate('/profile')}>Mon profil</Button>
+                <Button inverted className="nav--link" onClick={handleLogout}>Déconnexion</Button>
+            </div>
     );
 }
+
+HomePageLinks.propTypes = {
+    handleLogout: PropTypes.func.isRequired,
+};
 
 export default HomePageLinks;
