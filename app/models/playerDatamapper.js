eeaefe65408316a4c1b87ec5604d906d9865631c
Victor <vict74.ferreira@gmail.com>
2022-06-16 15:38:01 +0200

    feat: correction and test a creation game

--- app/models/playerDatamapper.js
+++ app/models/playerDatamapper.js
@@ -2,11 +2,11 @@ const client = require('./client');
 
 const playerDatamapper = {
 
-   async insert(gameId, playerId) {
+   async insert(gameId, dataPlayerId) {
 
       const preparedQuery = {
       text : `INSERT INTO "participation" ("game_id", "player_id") VALUES ($1, $2)`,
-      values: [gameId, playerId]
+      values: [gameId, dataPlayerId]
       }
 
       const newPlayer = await client.query(preparedQuery);
