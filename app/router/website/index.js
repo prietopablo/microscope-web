e5d0ef09825d01c6585f56a33d5dbc7ec8da6843
Victor <vict74.ferreira@gmail.com>
2022-06-17 14:54:45 +0200

    fix: merge back_card_creation

--- app/router/website/index.js
+++ app/router/website/index.js
@@ -24,14 +24,30 @@ router
    .patch(auth, userController.update)
    .delete(auth, userController.delete);
 
+// Check token validity
+router.post('/tokenValidity', authController.verifyToken);
+
 // Logout, check what solution front dev have
 router.post("/logout", auth, authController.logout);
 
 // New game creation
-router.post('/createNewGame', gameController.createNewGame);
+router.post('/createNewGame', auth, gameController.createNewGame);
+
+// When a game is created, user needs to be redirected to the game page/tabletop with :id
+router
+   .route('/game/:id')
+   .get(gameController.getOne)
+   // The following route is meant to update the current with game with all the data needed to start a game
+   .post(gameController.deployGame)
+   // Players can create new card
+   .post(/*gameController.createCard*/)
+   // Finishing game where you can choose to update state to "finished" or "archived"
+   .post(/*gameController.finishGame*/);
 
 // Game archive
+// response need to send game.id, game.bigpicture of *, access all the game with state "archived"
 router.get('/archive');
+// response need to send all the game, all the cards, all the data related to the game with :id
 router.get('/archive/:id');
 
 // Contact
