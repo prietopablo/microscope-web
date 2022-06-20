38f17c87f5c08f3e088624500acee33e243b4e1e
PrietoPablo <prietopablo88@gmail.com>
2022-06-20 14:34:41 +0200

    fix: trying to create card, problem with while loop

--- app/controllers/cardController.js
+++ app/controllers/cardController.js
@@ -5,7 +5,26 @@ const cardController = {
    async create (request, response) {
 
       try {
-            
+            console.log(request.body);
+
+            // First, we need to find if there is a previous_card_position
+            // we will use the findByPosition            
+            const cardInDesiredPosition = await cardDatamapper.findByPosition(request.body);
+            console.log("parent", parent);
+
+            while (cardInDesiredPosition) {
+                  await cardDatamapper.updatePosition(parent, request.body.cardType);
+                  
+                  const newcard = {
+                        cardType: request.body.cardType, index,
+                        parentType: request.body.parentType,
+                        parentId: parent.id,
+                        previous_card_position: parent.postion
+                  };
+
+                  cardInDesiredPosition = await cardDatamapper.findByPosition(newcard);
+            }
+            console.log("postwhile");
             await cardDatamapper.insert(request.body);
             // Check status code for error            
             return response.json({ Message: "Card creation succeed !"});
--- app/models/cardDatamapper.js
+++ app/models/cardDatamapper.js
@@ -2,40 +2,56 @@ const client = require('./client');
 
 const cardDatamapper = {
 
-   async insert(data) {
-     
-      console.log("data", data);
+   async findByPosition (data) {
 
-      const index = data.previous_card_position;
+      console.log("datamapper");
 
-      console.log("index", index);
+      const preparedQuery = {
+         text: `SELECT * FROM ${data.cardType} WHERE position = ${data.previous_card_position + 1} AND ${data.parentType}_id = ${data.parentId}`
+      };
 
-      const verifQuery = {
-         text: "SELECT * FROM $1 WHERE position = $2",
-         values: [data.cardType, index + 1]
-      }
-
-      const result = await client.query(verifQuery);
-
-      console.log(result.rows[0]);
-
-      // while (result.rows[0]) {
-
-      //    index++;
-
-      //    await client.query("UPDATE $1 SET position = $2 WHERE id = $3", [data.cardType, index + 1, result.rows[0].id]);
-         
-      //    result = await client.query("SELECT * FROM $1 WHERE position = $2", [data.cardType, index + 1]);
-
-      // }
-  
       // const preparedQuery = {
-      //    text: `INSERT INTO $1 ("text", "tone", "position", "$2") VALUES ($3, $4, $5, $6)`,
-      //    values: [data.cardType, "game_id", data.text, data.previous_card_position + 1, data.game_id]
+      //    text: `SELECT * FROM $1 WHERE position = $2 AND $3 = $4`,
+      //    values : [data.cardType, data.previous_card_position, `${data.parentType}_id`, data.parentId]
       // };
 
-      // await client.query(preparedQuery);
-   }
+      // const result = await client.query('SELECT * FROM $1 WHERE position = $2 AND $3 = $4',
+      // [data.cardType, data.previous_card_position, `${data.parentType}_id`, data.parentId]);
+
+      console.log("preparedQuery", preparedQuery);
+
+      const result = await client.query(preparedQuery);
+
+      console.log("result", result.rows[0]);      
+
+      return result.rows[0];
+   },
+
+   async insert (data) {
+     
+      console.log("datainsert", data);
+
+      const preparedQuery = {
+         text: `INSERT INTO $1 ("text", "tone", "position", "$2") VALUES ($3, $4, $5, $6)`,
+         values: [data.cardType, "game_id", data.text, data.previous_card_position + 1, data.game_id]
+      };
+
+      await client.query(preparedQuery);
+
+   },
+
+   async updatePosition (data, cardType) {
+
+      const savedGame = await client.query(
+          `UPDATE $1 SET
+            position = $2
+            WHERE $3 = $4
+            RETURNING *`,
+          [cardType, data.position + 1, `${data.parentType}_id`, data.parentId ],
+      );
+   
+      return savedGame.rows[0];
+   }   
 }
 
 module.exports = cardDatamapper;
