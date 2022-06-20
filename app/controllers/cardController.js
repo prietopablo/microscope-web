7a2e404c97263a4f6bf4e359c81ecb96ff5aa742
PrietoPablo <prietopablo88@gmail.com>
2022-06-20 17:24:18 +0200

    feat: card creation works for period table, we need to test it for other table

--- app/controllers/cardController.js
+++ app/controllers/cardController.js
@@ -5,32 +5,48 @@ const cardController = {
    async create (request, response) {
 
       try {
-            console.log(request.body);
+            //console.log("request.body", request.body);
 
             // First, we need to find if there is a previous_card_position
             // we will use the findByPosition            
-            const cardInDesiredPosition = await cardDatamapper.findByPosition(request.body);
-            console.log("parent", parent);
+            const cardsToMove = await cardDatamapper.findByPosition(request.body);
+            //console.log("cardsToMove", cardsToMove);
 
-            while (cardInDesiredPosition) {
-                  await cardDatamapper.updatePosition(parent, request.body.cardType);
+
+            if (cardsToMove) {
+                  let newcardPosition = request.body.previous_card_position + 1;
+
+                  for (let i = 0; i < cardsToMove.length; i++) {
+                        await cardDatamapper.updatePosition(request.body.cardType, cardsToMove[i].id, cardsToMove[i].position + 1);
+                  }
+                  // cardsToMove.forEach( async (card) => {
+                  //       newcardPosition + 1;
+                  //       console.log("newcardPosition", newcardPosition);
+                  //       await cardDatamapper.updatePosition(request.body.cardType, card.id, newcardPosition);
+                  // });
                   
-                  const newcard = {
-                        cardType: request.body.cardType, index,
-                        parentType: request.body.parentType,
-                        parentId: parent.id,
-                        previous_card_position: parent.postion
-                  };
-
-                  cardInDesiredPosition = await cardDatamapper.findByPosition(newcard);
             }
+            // while (cardInDesiredPosition) {
+            //       await cardDatamapper.updatePosition(cardInDesiredPosition, request.body.cardType);
+                  
+            //       const newcard = {
+            //             cardType: request.body.cardType, index,
+            //             parentType: request.body.parentType,
+            //             parentId: parent.id,
+            //             previous_card_position: parent.postion
+            //       };
+
+            //       cardInDesiredPosition = await cardDatamapper.findByPosition(newcard);
+            // }
             console.log("postwhile");
             await cardDatamapper.insert(request.body);
             // Check status code for error            
             return response.json({ Message: "Card creation succeed !"});
 
-      } catch {
-            return response.json({ errorMessage: "Card creation failed"});
+      // } catch (err) {
+      } catch (err) {
+            // return response.json({ errorMessage: "Card creation failed"});
+            return response.json(err.message);
       }
    }
 }
