78868882a56c2b7c1d6de97722473f678a5c085d
PrietoPablo <prietopablo88@gmail.com>
2022-06-20 17:24:18 +0200

    feat: card creation works for period table, we need to test it for other table

--- app/models/cardDatamapper.js
+++ app/models/cardDatamapper.js
@@ -7,12 +7,17 @@ const cardDatamapper = {
       console.log("datamapper");
 
       const preparedQuery = {
-         text: `SELECT * FROM ${data.cardType} WHERE position = ${data.previous_card_position + 1} AND ${data.parentType}_id = ${data.parentId}`
+         text: `SELECT * FROM ${data.cardType} WHERE position >= ${data.previous_card_position + 1} AND ${data.parentType}_id = ${data.parentId}`
       };
 
+      // make it more secure
       // const preparedQuery = {
-      //    text: `SELECT * FROM $1 WHERE position = $2 AND $3 = $4`,
-      //    values : [data.cardType, data.previous_card_position, `${data.parentType}_id`, data.parentId]
+      //    text: `SELECT * FROM ${data.cardType} WHERE position = $1 AND $2 = $3`,
+      //    values : [
+      //       // data.cardType,
+      //       data.previous_card_position + 1,
+      //       `${data.parentType}_id`,
+      //       data.parentId]
       // };
 
       // const result = await client.query('SELECT * FROM $1 WHERE position = $2 AND $3 = $4',
@@ -22,35 +27,50 @@ const cardDatamapper = {
 
       const result = await client.query(preparedQuery);
 
-      console.log("result", result.rows[0]);      
+      console.log("result", result.rows);      
 
-      return result.rows[0];
+      return result.rows;
    },
 
    async insert (data) {
      
       console.log("datainsert", data);
 
+      const parentType = `${data.parentType}_id`;
+      const positionToUpdate = data.previous_card_position + 1;
+      
+      console.log("positionToUpdate", positionToUpdate);
+
       const preparedQuery = {
-         text: `INSERT INTO $1 ("text", "tone", "position", "$2") VALUES ($3, $4, $5, $6)`,
-         values: [data.cardType, "game_id", data.text, data.previous_card_position + 1, data.game_id]
+         text: `INSERT INTO ${data.cardType} ("text", "tone", "position", "${parentType}") VALUES ('${data.text}', '${data.tone}', '${positionToUpdate}', '${data.parentId}')`
       };
+
+      console.log("preparedQuery", preparedQuery);
+
+
+      // const preparedQuery = {
+      //    text: `INSERT INTO $1 ("text", "tone", "position", "$2") VALUES ($3, $4, $5, $6)`,
+      //    values: [data.cardType, data.tone, `${data.parenType}_id`, data.text, data.previous_card_position + 1, data.game_id, data.parentId]
+      // };
 
       await client.query(preparedQuery);
 
    },
 
-   async updatePosition (data, cardType) {
+   async updatePosition (cardType, cardId, newPosition) {
 
-      const savedGame = await client.query(
-          `UPDATE $1 SET
-            position = $2
-            WHERE $3 = $4
+      const preparedQuery = {
+         text: `UPDATE ${cardType} SET
+            position = $1
+            WHERE id = $2
             RETURNING *`,
-          [cardType, data.position + 1, `${data.parentType}_id`, data.parentId ],
-      );
+          values: [newPosition, cardId],
+      };
+      console.log(preparedQuery);
+
+      const savedCard = await client.query(preparedQuery);
    
-      return savedGame.rows[0];
+      return savedCard.rows[0];
    }   
 }
 
